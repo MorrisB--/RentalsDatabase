0e9a2b958299670f38076490d4b894298795f0b1
MorrisB-- <morris.ballenger@yahoo.com>
2016-04-23 18:26:36 -0500

    Took off all testing code

--- src/ConnectionToDatabase.java
+++ src/ConnectionToDatabase.java
@@ -19,24 +19,24 @@ public class ConnectionToDatabase {
 			connection = DriverManager.getConnection("jdbc:mysql://cs.neiu.edu:3306/db_Spr16_" + username + "?user="
 					+ username + "&password=" + password);
 
-			String sql1 = "INSERT INTO Breakfast VALUES (11, 'Syrup')";
-			String sql2 = "INSERT INTO Breakfast VALUES (12, 'Extra Salt')";
-			String sql3 = "DELETE FROM Breakfast WHERE id = 12";
-			String sql4 = "INSERT INTO Breakfast VALUES (12, 'Light Salt')";
-			String sql5 = "SELECT * FROM Breakfast";
+		//	String sql1 = "INSERT INTO Breakfast VALUES (11, 'Syrup')";
+			//String sql2 = "INSERT INTO Breakfast VALUES (12, 'Extra Salt')";
+			//String sql3 = "DELETE FROM Breakfast WHERE id = 12";
+			//String sql4 = "INSERT INTO Breakfast VALUES (12, 'Light Salt')";
+			//String sql5 = "SELECT * FROM Breakfast";
 
 			Statement statement1 = connection.createStatement();
-			statement1.executeUpdate(sql1);
-			statement1.executeUpdate(sql2);
-			statement1.executeUpdate(sql3);
-			statement1.executeUpdate(sql4);
+			//statement1.executeUpdate(sql1);
+			//statement1.executeUpdate(sql2);
+			//statement1.executeUpdate(sql3);
+			//statement1.executeUpdate(sql4);
 
-			System.out.println("\n" + sql5 + "\nid name");
-			ResultSet rs = statement1.executeQuery(sql5);
-			while (rs.next()) {
-				String all = rs.getString("id") + "  " + rs.getString("name");
-				System.out.println(all);
-			}
+			//System.out.println("\n" + sql5 + "\nid name");
+			//ResultSet rs = statement1.executeQuery(sql5);
+			//while (rs.next()) {
+			//	String all = rs.getString("id") + "  " + rs.getString("name");
+			//	System.out.println(all);
+			//	}
 
 		} catch (SQLException e) {
 			
