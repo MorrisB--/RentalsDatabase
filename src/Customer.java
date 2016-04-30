2c4a1224f10215b636335a3f062b42fc21f007c2
MorrisB-- <morris.ballenger@yahoo.com>
2016-04-29 20:06:13 -0500

    Created Customer.java and added createCustomer(String, String, String, String) method

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,11 @@
 
-public class Customer {
+public class Customer extends ConnectionToDatabase {
+
+	public void createCustomer(String firstName, String lastName, String address, String phoneNumber) {
+
+		String createCustomer = "INSERT INTO Customer VALUES (" + (this.getRows() + 1) + ", '" + firstName + "', '"
+				+ lastName + "', '" + address + "', '" + phoneNumber + "');";
+
+	}
 
 }
