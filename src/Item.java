4e62c1ba6f1967df550103eb38257b53148b667c
MorrisB-- <morris.ballenger@yahoo.com>
2016-05-01 13:42:39 -0500

    Finished createItem(S, d, i), updated todo

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,23 @@
+import java.sql.*;
 
-public class Item {
+public class Item extends ConnectionToDatabase {
+	
+	public void createItem(String name, double price, int lateFee){
+		
+		String insertItem = "INSERT INTO Item VALUES(" + name + ", " + price + ", " + lateFee + ");";
+		
+		try {
+
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(insertItem);
+			System.out.println(name +  " has been succesfully added to the system.");
+
+		} catch (SQLException e) {
+			System.out.println("SQLException: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("SQLException: " + e.getErrorCode());
+		}
+		
+	}
 
 }
